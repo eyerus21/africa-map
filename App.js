876b076c942c97a38c8caace7f8ea20cad0a9a7a
Eyerusalem <50240801+eyerus21@users.noreply.github.com>
2020-10-24 13:18:40 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+     
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
